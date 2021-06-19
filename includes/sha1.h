20e07458d996e6c293da8086d2a8eb5b52864c90
DasIstVeraltet <dehibeo@gmail.com>
2021-06-20 02:26:53 +0300

    [+] Added doxygen

--- includes/sha1.h
+++ includes/sha1.h
@@ -1,3 +1,22 @@
+/****************************INFORMATION***********************************
+* Copyright (c) 2021 Zontec
+* Email: dehibeo@gmail.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+**************************************************************************/
+/*!
+*   @file sha1.h
+*   @brief File contains SHA1 API functions.
+*	@author Zontec
+*	@version 1.1
+*	@date 2021.06.19
+*/
+
 #ifndef SHA1_H
 #define SHA1_H
 
@@ -18,13 +37,44 @@ typedef struct sha1_t
 } sha1_t;
 
 
+/**
+ * @brief Function inits sha1 context with default values
+ * 
+ * @param[in,out] ctx SHA context to be initialized
+ * @return security_status_e 
+ */
 security_status_e SECURITY_API sha1_init(sha1_t *ctx);
 
+/**
+ * @brief Function updates the context with new hash computations based on 
+ * input data.
+ * 
+ * @param[in,out] ctx SHA1 context
+ * @param[in] data Input data to be hashed
+ * @param[in] data_len Input data len
+ * @return security_status_e 
+ */
 security_status_e SECURITY_API sha1_update(sha1_t *ctx, 
                                         const uint8_t *data, uint32_t data_len);
 
+/**
+ * @brief Function finishes computations and produce final hash
+ * 
+ * @param[in] ctx SHA1 context
+ * @param[out] out Output hash. Should be at least allocated SHA1_HASH_SIZE memory
+ * @return security_status_e 
+ */
 security_status_e SECURITY_API sha1_finish(sha1_t *ctx, uint8_t *out);
 
+/**
+ * @brief Function implements step be step three functions: 
+ * sha1_init, sha1_update, sha1_finish
+ * 
+ * @param[in] data Input data to be hashed
+ * @param[in] data_len Input data len
+ * @param[out] out Output hash. Should be at least allocated SHA1_HASH_SIZE memory
+ * @return security_status_e 
+ */
 security_status_e SECURITY_API sha1(const uint8_t *data, 
                                         uint32_t data_len, uint8_t *out);
 
