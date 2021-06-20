9a31a2a4e7d9481af3a31b8fc247208cf1f5824a
DasIstVeraltet <dehibeo@gmail.com>
2021-06-20 16:57:25 +0300

    [+] Added doxygen

--- includes/security.h
+++ includes/security.h
@@ -1,5 +1,24 @@
-#ifndef __SECURITY_H__
-#define __SECURITY_H__
+/****************************INFORMATION***********************************
+* Copyright (c) 2021 Zontec
+* Email: dehibeo@gmail.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+**************************************************************************/
+/*!
+*   @file security.h
+*   @brief File contains important function definitions for security library.
+*	@author Zontec
+*	@version 1.1
+*	@date 2021.06.19
+*/
+
+#ifndef SECURITY_H
+#define SECURITY_H
 
 #include <stdlib.h>
 #include <stdint.h>
@@ -8,15 +27,58 @@
 #include "security_defines.h"
 #include "security_configs.h"
 
-#define memset_safe memset
+/**
+ * @brief Should be defined a safe function for memset with no return value.
+ * By default: memset
+ */
+#ifndef memset_safe
+#define memset_safe                               memset
+#endif 
 
-#define MAX_BYTE_VALUE                                  0xFF
-#define MAX_SHORT_VALUE                                 0xFFFF
-#define MAX_WORD_VALUE                                  0xFFFFFFFF
-#define MAX_DWORD_VALUE                                 0xFFFFFFFFFFFFFFFF
+#define MAX_BYTE_VALUE                            0xFF
+#define MAX_SHORT_VALUE                           0xFFFF
+#define MAX_WORD_VALUE                            0xFFFFFFFF
+#define MAX_DWORD_VALUE                           0xFFFFFFFFFFFFFFFF
+
+#define SECURITY_API
+
+#define SECURITY_FUNCTION_EXIT                    sec_exit
+#define SECURITY_FUNCTION_RET_VAR                 sec_ret
+#define SECURITY_FUNCTION_BEGIN                   security_status_e SECURITY_FUNCTION_RET_VAR = \
+                                                    SECURITY_STATUS_OK;
+#define SECURITY_FUNCTION_RETURN                  return SECURITY_FUNCTION_RET_VAR;
+
+/**
+ * @brief Macro for checking pointer value. if NULL then jumps SECURITY_FUNCTION_EXIT
+ */
+#define SECURITY_CHECK_VALID_NOT_NULL(x)        \
+do{                                             \
+    if ((x) == NULL)                            \
+    {                                           \
+        SECURITY_FUNCTION_RET_VAR =             \
+        SECURITY_STATUS_FAIL_NULL_PTR;          \
+        goto SECURITY_FUNCTION_EXIT;            \
+    }                                           \
+} while (0)                                     \
+
+/**
+ * @brief Macro for checking security function result. 
+ * if not SECURITY_STATUS_OK then jumps SECURITY_FUNCTION_EXIT
+ */
+#define SECURITY_CHECK_RES(x)                   \
+do{                                             \
+    if (SECURITY_FUNCTION_RET_VAR = (x)         \
+        != SECURITY_STATUS_OK)                  \
+    {                                           \
+        goto SECURITY_FUNCTION_EXIT;            \
+    }                                           \
+} while (0)                                     \
 
 
-typedef enum security_status_e
+/**
+ * @brief Contains enums with error status codes for security functions
+ */
+typedef enum 
 {
     SECURITY_STATUS_OK =                                0x00000000,
     SECURITY_STATUS_FAIL =                              0x0000000A,
@@ -26,36 +88,5 @@ typedef enum security_status_e
     SECURITY_STATUS_FAIL_INCORRECT_FUNCTION_PARAM =     0x0000000E,
 } security_status_e;
 
-#define SECURITY_API
 
-#define SECURITY_FUNCTION_EXIT                    sec_exit
-
-#define SECURITY_FUNCTION_RET_VAR                 sec_ret
-
-#define SECURITY_FUNCTION_BEGIN                   security_status_e SECURITY_FUNCTION_RET_VAR = \
-                                                    SECURITY_STATUS_OK;
-
-#define SECURITY_FUNCTION_RETURN                  return SECURITY_FUNCTION_RET_VAR;
-
-
-#define SECURITY_CHECK_VALID_NOT_NULL(x)    \
-do{                                         \
-    if ((x) == NULL)                        \
-    {                                       \
-        SECURITY_FUNCTION_RET_VAR =         \
-        SECURITY_STATUS_FAIL_NULL_PTR;      \
-        goto SECURITY_FUNCTION_EXIT;        \
-    }                                       \
-} while (0)                                 \
-
-
-#define SECURITY_CHECK_RES(x)               \
-do{                                         \
-    if (SECURITY_FUNCTION_RET_VAR = (x)     \
-        != SECURITY_STATUS_OK)              \
-    {                                       \
-        goto SECURITY_FUNCTION_EXIT;        \
-    }                                       \
-} while (0)                                 \
-
-#endif /*__SECURITY_H__*/
+#endif /* SECURITY_H */
