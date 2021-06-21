1550385e32b83e025b8f7c952e9afb076a410ed8
DasIstVeraltet <dehibeo@gmail.com>
2021-06-21 23:09:32 +0300

    [+] Added security utils

---
+++ includes/security_utils.h
@@ -0,0 +1,8 @@
+#ifndef SECURITY_UTILS_H
+#define SECURITY_UTILS_H
+
+#include "security.h"
+
+void mem_xor_secured(uint8_t *dst, const uint8_t *src, uint32_t size);
+
+#endif /* SECURITY_UTILS_H */
