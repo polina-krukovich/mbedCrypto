b09aaf8c1fa143749f9517eacd506827979232d5
DasIstVeraltet <dehibeo@gmail.com>
2021-06-22 14:42:09 +0300

    [~] Added #endif

--- includes/security.h
+++ includes/security.h
@@ -33,8 +33,11 @@
  */
 #ifndef memset_safe
 #define memset_safe                               memset
+#endif 
 
+#ifndef mem_xor
 #define mem_xor                                   mem_xor_secured
+#endif 
 
 #define MAX_BYTE_VALUE                            0xFF
 #define MAX_SHORT_VALUE                           0xFFFF
