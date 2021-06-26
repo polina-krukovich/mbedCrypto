45cf8e3521d7bd1edd0130a73b1e24bd1594dec7
DasIstVeraltet <dehibeo@gmail.com>
2021-06-26 23:54:19 +0300

    Add mem_xor preproc error

--- hmac_sha512.c
+++ hmac_sha512.c
@@ -4,9 +4,9 @@
 #define OPAD_BYTE           (U8(0x5C))
 
 #if (HMAC_SHA512_MIN_SIZE == ENABLED)
-#ifndef mem_xor
-    #error "mem_xor not defined. See mem_xor interface in security.h"
-#endif /* mem_xor */
+    #ifndef mem_xor
+        #error "mem_xor not defined. See mem_xor interface in security.h"
+    #endif /* mem_xor */
 #endif /* HMAC_SHA512_MIN_SIZE */
 
 security_status_e hmac_sha512_init(hmac_sha512_t *ctx, const uint8_t *key, 
