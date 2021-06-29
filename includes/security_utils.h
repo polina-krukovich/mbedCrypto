f2becda1887c1173f1e87ec1cfd3cef917b075b0
DasIstVeraltet <dehibeo@gmail.com>
2021-06-29 22:31:37 +0300

    [~] __cplusplus

--- includes/security_utils.h
+++ includes/security_utils.h
@@ -3,6 +3,14 @@
 
 #include "security.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void mem_xor_secured(uint8_t *dst, const uint8_t *src, uint32_t size);
+
+#ifdef __cplusplus
+}
+#endif /*__cplusplus*/
 
 #endif /* SECURITY_UTILS_H */
