e5de6bb8aec4fd52c04bd2d6b7ccbae62215c2c7
DasIstVeraltet <dehibeo@gmail.com>
2021-07-16 14:10:31 +0300

    [~] Aes rework inreface. used structs instead of params

--- includes/aes.h
+++ includes/aes.h
@@ -21,6 +21,7 @@ typedef enum aes_mode_e
     AES_CTR =   4,
     AES_GCM =   5,
     AES_AEAD =  6,
+    AES_XTS  =  7,
 } aes_mode_e;
 
 typedef enum aes_key_expansion_hash_type_e
@@ -37,64 +38,33 @@ typedef struct aes_key_t
     aes_type_e aes_type;
 } aes_key_t;
 
+typedef struct aes_input_t
+{
+    uint8_t *key;
+    uint32_t *key_len;
+    uint8_t *data;
+    uint32_t *data_len;
+    uint8_t *iv;
+    uint32_t *iv_len;
+    uint8_t *auth_data;
+    uint32_t auth_data_len;
+} aes_input_t;
+
+typedef struct aes_output_t
+{
+    uint8_t *out;
+    uint32_t out_len;
+    uint8_t *tag;
+    uint32_t tag_len;
+} aes_output_t;
 
 security_status_e aes_key_init(aes_type_e aes_type, aes_key_t *aes_key);
 
 security_status_e aes_key_expand(aes_key_expansion_hash_type_e key_exp_hash_type,
                                     const uint8_t *key_in, uint32_t key_in_len, aes_key_t *key_out);
 
-security_status_e aes_encrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_decrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                    uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_cbc_encrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_cbc_decrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_ofb_encrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_ofb_decrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_cfb_encrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_cfb_decrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_ctr_encrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_ctr_decrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_aead_encrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_aead_decrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_gcm_encrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-security_status_e aes_gcm_decrypt_block(aes_key_t *key, const uint8_t in[AES_BLOCK_SIZE], 
-                                        const uint8_t iv[AES_BLOCK_SIZE], uint8_t out[AES_BLOCK_SIZE]);
-
-
-security_status_e aes_ecb_encrypt(const uint8_t *data, uint32_t data_len, aes_key_t *key, uint8_t *out);
-
-security_status_e aes_ecb_decrypt(const uint8_t *data, uint32_t data_len, aes_key_t *key, uint8_t *out);
-
 security_status_e aes_ecb_encrypt_ex(aes_type_e aes_type, aes_key_expansion_hash_type_e key_exp_hash_type, 
                                 const uint8_t *data, uint32_t data_len, 
-                                uint8_t *key, uint32_t key_len, uint8_t *out);
-
-security_status_e aes_ecb_decrypt_ex(aes_type_e aes_type, aes_key_expansion_hash_type_e key_exp_hash_type, 
-                                const uint8_t *c_data, uint32_t c_data_len, 
                                 uint8_t *key, uint32_t key_len, uint8_t *out);
 
 void aes_key_free(aes_key_t *aes_key);
