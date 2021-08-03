655638c6a7254d76524df0475520271a3c63de69
DasIstVeraltet <dehibeo@gmail.com>
2021-08-04 00:50:17 +0300

    [+] Added header description

--- src/aes.c
+++ src/aes.c
@@ -1,3 +1,22 @@
+/****************************INFORMATION***********************************
+* Copyright (c) 2021 Zontec
+* Email: dehibeo@gmail.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+**************************************************************************/
+/*!
+*   @file aes.c
+*   @brief File contains AES implementation.
+*	@author Zontec
+*	@version 1.1
+*	@date 2021.07.04
+*/
+
 #include "aes.h"
 
 #include "sha1.h"
