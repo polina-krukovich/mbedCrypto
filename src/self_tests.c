359939712deea0795dc05a7b1b3cefcd319e45b0
DasIstVeraltet <dehibeo@gmail.com>
2021-08-03 23:52:55 +0300

    [+] Added tests defines + interfaces

--- src/self_tests.c
+++ src/self_tests.c
@@ -13,7 +13,7 @@
 #include "sha256.h"
 #include "sha512.h"
 #include "pbkdf2.h"
-#include "rand.h"
+#include "drbg.h"
 
 #include "hmac_sha1.h"
 #include "hmac_sha256.h"
@@ -44,6 +44,30 @@ char sha_test[][DATA_SIZE] =
     "flkds;lafddddddddddddddddddddddddddddddddddddddddddlfklskdlfksldkflsdfkld",
 };
 const uint8_t sha_test_len = sizeof(sha_test)/DATA_SIZE;
+
+
+#define TEST_SHA1
+#define TEST_SHA256
+#define TEST_SHA512
+#define TEST_HMAC_SHA1
+#define TEST_HMAC_SHA256
+#define TEST_HMAC_SHA512
+#define TEST_PBKDF2
+#define TEST_KBKDF
+#define TEST_DRBG
+#define TEST_ENTROPY
+#define TEST_RSA
+#define TEST_AES
+#define TEST_RC4
+#define TEST_BLOWFISH
+#define TEST_CHACHA
+#define TEST_SALSA20
+#define TEST_DH
+
+
+
+#if defined(TEST_SHA1)
+
 void test_sha1()
 {
     printf("SHA1 test\n");
@@ -72,6 +96,11 @@ void test_sha1()
     printf("sha1 tests finished\n");
     assert(cnt == sha_test_len);
 }
+
+#endif /* TEST_SHA1 */
+
+
+#if defined(TEST_SHA256)
 
 void test_sha256()
 {
@@ -102,6 +131,11 @@ void test_sha256()
     assert(cnt == sha_test_len);
 }
 
+#endif /* TEST_SHA256 */
+
+
+#if defined(TEST_SHA512)
+
 void test_sha512()
 {
     printf("SHA512 test\n");
@@ -130,6 +164,11 @@ void test_sha512()
     printf("sha512 tests finished\n");
     assert(cnt == sha_test_len);
 }
+
+#endif /* TEST_SHA512 */
+
+
+#if defined(TEST_HMAC_SHA1)
 
 hmac_sha1_t hmac1;
 
@@ -182,6 +221,11 @@ void test_hmac_sha1()
     assert(cnt == sha_test_len);
 }
 
+#endif /* TEST_HMAC_SHA1 */
+
+
+#if defined(TEST_HMAC_SHA256)
+
 void test_hmac_sha256()
 {
     printf("HMAC_sha256 test\n");
@@ -210,6 +254,11 @@ void test_hmac_sha256()
     printf("HMAC_sha256 tests finished\n");
     assert(cnt == sha_test_len);
 }
+
+#endif /* TEST_HMAC_SHA256 */
+
+
+#if defined(TEST_HMAC_SHA512)
 
 void test_hmac_sha512()
 {
@@ -240,6 +289,10 @@ void test_hmac_sha512()
     assert(cnt == sha_test_len);
 }
 
+#endif /* TEST_HMAC_SHA512 */
+
+
+#if defined(TEST_PBKDF2)
 
 void test_pbkdf2_hmac_sha1()
 {
@@ -320,57 +373,221 @@ void test_pbkdf2_hmac_sha512()
     assert(cnt == sha_test_len);
 }
 
-int abs(int a)
+#endif /* TEST_PBKDF2 */
+
+
+#if defined(TEST_KBKDF)
+
+void test_kbkdf_ctr()
 {
-    return a >= 0 ? a : -a;
+
 }
+
+void test_kbkdf_fb()
+{
+    
+}
+
+void test_kbkdf_dp()
+{
+    
+}
+#endif /* TEST_KBKDF */
+
+#ifdef RRR
+
+void aes_tests()
+{
+    uint8_t out1[128];
+
+    uint8_t data[] = "1234567812345678123456781234567812345432234234234234234332";
+    uint8_t iv[] = "1234567812345678";
+    uint8_t key[] = "1234567812345678";
+
+    aes_input_t in;
+    aes_output_t out;
+
+    in.data = data;
+    in.data_len = sizeof(data) - 1;
+
+    in.iv = iv;
+    in.iv_len = sizeof(iv) - 1;
+
+    in.key = key;
+    in.key_len = sizeof(key) - 1;
+
+    out.out = out1;
+/* ECB */
+    printf("##########################################################\n");
+    printf("AES ECB test\n");
+
+    memset(out.out, 0, 128);
+    aes_encrypt(AES_ECB, AES128, AES_KEY_EXPANSION_NOT_REQUIRED, &in, &out);
+    printf("Res: ");
+    print_arr(out.out, 64);
+    printf("exp: ");
+    printf("6DAC1C56E747FAE03ACF8C6891E428E06DAC1C56E747FAE03ACF8C6891E428E0D176DCCE30D0E5B0E5A1D726E706F7F16D4878ABC1B9FC4EB40C8AF75D4D5577\n");
+
+/* CBC */ 
+    printf("##########################################################\n");
+    printf("AES CBC test\n");
+
+    memset(out.out, 0, 128);
+    aes_encrypt(AES_CBC, AES128, AES_KEY_EXPANSION_NOT_REQUIRED, &in, &out);
+    printf("Res: ");
+    print_arr(out.out, 64);
+    printf("exp: ");
+    printf("6DAC1C56E747FAE03ACF8C6891E428E06DAC1C56E747FAE03ACF8C6891E428E0D176DCCE30D0E5B0E5A1D726E706F7F16D4878ABC1B9FC4EB40C8AF75D4D5577\n");
+
+
+/* CFB */ 
+    printf("##########################################################\n");
+    printf("AES CFB test\n");
+
+    memset(out.out, 0, 128);
+    aes_encrypt(AES_CFB, AES128, AES_KEY_EXPANSION_NOT_REQUIRED, &in, &out);
+    printf("Res: ");
+    print_arr(out.out, 64);
+    printf("exp: ");
+    printf("9AE8FD02B340288A0E7BBFF0F0BA54D67ABDAC9471AF10FB31CC31D23845D53E9728A7FDABB4D17DD6EC55090A39490A0D32898226DE6A9D02116889CC79977F\n");
+
+/* OFB */ 
+    printf("##########################################################\n");
+    printf("AES OFB test\n");
+
+    memset(out.out, 0, 128);
+    aes_encrypt(AES_OFB, AES128, AES_KEY_EXPANSION_NOT_REQUIRED, &in, &out);
+    printf("Res: ");
+    print_arr(out.out, 64);
+    printf("exp: ");
+    printf("9AE8FD02B340288A0E7BBFF0F0BA54D67ABDAC9471AF10FB31CC31D23845D53E9728A7FDABB4D17DD6EC55090A39490A0D32898226DE6A9D02116889CC79977F\n");
+
+
+/* CTR */ 
+    printf("##########################################################\n");
+    printf("AES CTR test\n");
+
+    memset(out.out, 0, 128);
+    aes_encrypt(AES_CTR, AES128, AES_KEY_EXPANSION_NOT_REQUIRED, &in, &out);
+    printf("Res: ");
+    print_arr(out.out, 64);
+    printf("exp: ");
+    printf("9AE8FD02B340288A0E7BBFF0F0BA54D67ABDAC9471AF10FB31CC31D23845D53E9728A7FDABB4D17DD6EC55090A39490A0D32898226DE6A9D02116889CC79977F\n");
+
+
+/* XTS */ 
+    printf("##########################################################\n");
+    printf("AES XTS test\n");
+
+    memset(out.out, 0, 128);
+    aes_encrypt(AES_XTS, AES128, AES_KEY_EXPANSION_NOT_REQUIRED, &in, &out);
+    printf("Res: ");
+    print_arr(out.out, 64);
+    printf("exp: ");
+    printf("9AE8FD02B340288A0E7BBFF0F0BA54D67ABDAC9471AF10FB31CC31D23845D53E9728A7FDABB4D17DD6EC55090A39490A0D32898226DE6A9D02116889CC79977F\n");
+}
+#endif
 
 int main()
 {
-    
+
+
+#if defined(TEST_SHA1)
     test_sha1();
+#endif
+
+
+#if defined(TEST_SHA256)
     test_sha256();
+#endif
+
+
+#if defined(TEST_SHA512)
     test_sha512();
+#endif
 
+
+#if defined(TEST_HMAC_SHA1)
     test_hmac_sha1();
-    test_hmac_sha256();
-    test_hmac_sha512();
+#endif
 
+
+#if defined(TEST_HMAC_SHA256)
+    test_hmac_sha256();
+#endif
+
+
+#if defined(TEST_HMAC_SHA512)
+    test_hmac_sha512();
+#endif
+
+
+#if defined(TEST_PBKDF2)
     test_pbkdf2_hmac_sha1();
     test_pbkdf2_hmac_sha256();
     test_pbkdf2_hmac_sha512();
-
-    srand(312);
-    int cnt[10] = {0};
-
-    uint8_t data[10000];
-    printf("%d", is_le());  
-    rand_bytes_ex(data, 10000, rand);
-    for (int i = 0; i < 10000; i ++)
-    {
-        cnt[abs(data[i])%10]++;
-    }
+#endif
 
 
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d ", cnt[i]);
-    }
-    printf("\n");
+#if defined(TEST_KBKDF)
+    test_kbkdf_ctr();
+    test_kbkdf_fb();
+    test_kbkdf_dp();
+#endif
 
-    uint8_t out[128];
-    uint8_t out1[128];
-    aes_key_t key;
 
-    uint8_t in[16] = "1234567812345678";
-    uint8_t iv[16] = "1234567812345678";
-    
+#if defined(TEST_DRBG)
+    test_drbg_ctr();
+    test_drbg_hash();
+    test_drbg_hmac();
+#endif
 
-    // aes_key_init(AES128, &key);
-    // aes_key_expand(AES_KEY_EXPANSION_NOT_REQUIRED, "1234567812345678", 16, &key);
-    // memset(out, 0, 128);
-    // aes_cfb_encrypt_block(&key, in,iv, out);
-    // print_arr(out, 32);
+
+#if defined(TEST_ENTROPY)
+    test_entropy();
+#endif
+
+
+#if defined(TEST_RSA)
+    test_rsa();
+    test_rsa_pss();
+#endif
+
+
+#if defined(TEST_AES)
+    test_aes_ecb();
+    test_aes_cbc();
+    test_aes_ofb();
+    test_aes_cfb();
+    test_aes_ctr();
+    test_aes_xts();
+    test_aes_aead();
+#endif
+
+
+#if defined(TEST_RC4)
+    test_rc4();
+#endif
+
+
+#if defined(TEST_BLOWFISH)
+    test_rc4();
+#endif
+
+
+#if defined(TEST_CHACHA)
+    test_chacha();
+#endif
+
+
+#if defined(TEST_SALSA20)
+    test_chacha();
+#endif
+
+
+#if defined(TEST_DH)
+    test_dh();
+#endif
 
   return 0;
 }   
