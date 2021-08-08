6bb5a9231deb52b92c8eeba10b6c18f80b2c36c9
DasIstVeraltet <dehibeo@gmail.com>
2021-08-08 19:09:37 +0300

    [+] Added RC4

--- src/rc4.c
+++ src/rc4.c
@@ -18,3 +18,94 @@
 */
 
 #include "rc4.h"
+
+
+#define ROUNDS                  (256)
+
+#define IS_POWER_OF_2(x)        !((x) & (x-1))
+
+#if IS_POWER_OF_2(ROUND)
+#define MOD(x)    & U32((x)-1)
+#else
+#define MOD(x)    % U32(x)
+#endif /* MOD */
+
+
+/* ONE_ROUND define */
+#define ONE_ROUND \
+        do {                                                            \
+            j = (j + U32(s[i]) + U32(key[i % key_len])) MOD(rounds);    \
+            swap8u(&s[i], &s[j]);                                       \
+            ++i;                                                        \
+        } while (0);
+
+
+static void swap8u(uint8_t *a, uint8_t *b) 
+{
+    uint8_t tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+static void ksa(uint8_t *key, uint32_t key_len, uint8_t *s) 
+{
+    for (uint32_t i = 0; i < ROUNDS; ++i)
+    {
+        s[i] = i;
+    }
+
+    uint32_t j = 0;
+    const uint32_t rounds = U32(ROUNDS);
+
+    for(uint32_t i = 0; i < rounds;) 
+    {
+#if defined(RC4_MIN_SIZE) && (ROUNDS % 2 == 0)
+        ONE_ROUND;
+        ONE_ROUND;
+#if (ROUNDS % 4 == 0)
+        ONE_ROUND;
+        ONE_ROUND;
+#endif
+#if (ROUNDS % 8 == 0)
+        ONE_ROUND;
+        ONE_ROUND;
+        ONE_ROUND;
+        ONE_ROUND;
+#endif
+#else
+        ONE_ROUND;
+#endif
+    }
+}
+
+
+void rc4(uint8_t *key, uint32_t key_len, uint8_t *data, uint32_t data_len, uint8_t *out, uint32_t skip) 
+{
+
+    uint8_t s[ROUNDS] = {0};
+
+    uint32_t k = 0;
+    uint32_t j = 0;
+    uint32_t t = 0;
+
+    const uint32_t rounds = U32(ROUNDS);
+
+    ksa(key, key_len, s);
+
+    while(skip--)
+    {
+        k = (k + 1) MOD(rounds);           
+        j = (j + s[k]) MOD(rounds);        
+        swap8u(&s[k], &s[j]);    
+    }
+ 
+    for (uint32_t i = 0; i < data_len; ++i)
+    {
+        k = (k + 1) MOD(rounds);           
+        j = (j + s[k]) MOD(rounds);        
+        swap8u(&s[k], &s[j]);  
+
+        t = s[(s[k] + s[j]) MOD(rounds)];
+        out[i] = t ^ data[i];
+    }
+}
